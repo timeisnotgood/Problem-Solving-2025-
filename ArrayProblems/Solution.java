4118af7d56f655e2d89b658490e23034c060cff7
bhoopathi <bhoopathicl@gmail.com>
2025-05-11 20:50:44 +0530

    prefix Sum

--- ArrayProblems/Solution.java
+++ ArrayProblems/Solution.java
@@ -20,12 +20,32 @@ public class Solution {
         return longest;
     }
 
+    void prefixSum(int[] arr, int que[][]){
+
+        int[] prefix = new int[arr.length];
+        prefix[0] = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            prefix[i] = arr[i] + prefix[i-1];
+        }
+
+        int longest = Integer.MAX_VALUE;
+
+        for(int[] num : que){
+            int right = num[1];
+
+            longest = Math.max(prefix[right], longest);
+        }
+
+        System.out.println(Integer.MAX_VALUE);
+    }
+
     public static void main(String[] args) {
         Solution obj = new Solution();
         
-        int[] arr = {2, 1, 5, 1, 3, 2};
-        System.out.println("---> " + 
-        obj.slidingWindow(arr, 3)
-        );
+        int arr[] = {1, 2, 3, 4, 5};
+        int[][] que = {{0, 2},{1, 3},{2, 4}};
+
+        obj.prefixSum(arr, que);
     }
 }
